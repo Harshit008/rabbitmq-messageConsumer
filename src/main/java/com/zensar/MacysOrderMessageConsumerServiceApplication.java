3458354c5131be0bb4feb91636ada63b3460b37d
Harshit <srivastavaharshit008@gmail.com>
2022-01-06 17:47:23 +0530

    Swagger config changes

--- src/main/java/com/zensar/MacysOrderMessageConsumerServiceApplication.java
+++ src/main/java/com/zensar/MacysOrderMessageConsumerServiceApplication.java
@@ -3,24 +3,24 @@ package com.zensar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import com.zensar.config.SwaggerConfiguration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAutoConfiguration
-@EnableSwagger2
+@Import(SwaggerConfiguration.class)
+@EnableEurekaClient
 @SpringBootApplication
 public class MacysOrderMessageConsumerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MacysOrderMessageConsumerServiceApplication.class, args);
-	}
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.zensar")).build();
 	}
 
 }
