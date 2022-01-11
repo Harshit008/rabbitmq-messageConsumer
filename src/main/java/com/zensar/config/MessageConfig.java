4ac2a3bed23fba8629e15054d501edaf179b9868
Harshit <srivastavaharshit008@gmail.com>
2022-01-11 18:06:22 +0530

    Configured remote rabbitmq server connection for queues

--- src/main/java/com/zensar/config/MessageConfig.java
+++ src/main/java/com/zensar/config/MessageConfig.java
@@ -1,6 +1,7 @@
 package com.zensar.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -89,6 +90,16 @@ public class MessageConfig {
         return rabbitTemplate;
     }
     
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("34.125.195.26");
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("admin");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/admin");
+        return connectionFactory;
+    }
     
     
 
