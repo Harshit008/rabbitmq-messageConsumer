d50e9a6a7cfaab113e8b5ea475c16231aebab5ca
Harshit <srivastavaharshit008@gmail.com>
2022-01-17 19:55:25 +0530

    Bugf fixes in Swagger config

--- src/main/java/com/zensar/config/SwaggerConfiguration.java
+++ src/main/java/com/zensar/config/SwaggerConfiguration.java
@@ -26,10 +26,10 @@ public class SwaggerConfiguration {
 
     private ApiInfo getApiInfo() {
         return new ApiInfoBuilder()
-                .title("Macys order service API")
+                .title("Macys message producer service API")
                 .version("1.0")
-                .description("API for Macys order service Application")
-                .contact(new Contact("Macys order service Application", "NA", "srivastavaharshit008@gmail.com"))
+                .description("API for Macys message producer application")
+                .contact(new Contact("Macys message producer application", "NA", "srivastavaharshit008@gmail.com"))
                 .license("Apache License Version 2.0")
                 .build();
     }
